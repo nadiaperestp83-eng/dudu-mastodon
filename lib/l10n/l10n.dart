b56b0ea07c5352435d0179a2ae7046e99a0bad6f
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-09-10 18:06:14 -0300

    Update l10n.dart

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -1,8 +1,3 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
-class S {
-  static AppLocalizations of(BuildContext context) {
-    return AppLocalizations.of(context);
-  }
-}
+arb-dir: lib/l10n
+template-arb-file: app_zh.arb
+output-localization-file: app_localizations.dart
